5f2a4909757a33d2c1319ec30b673d15854b5291
SpaYco <zizou.aziz101@gmail.com>
2020-02-13 15:48:16 +0100

    added header and navbar with mobile breakpoints

---
+++ Media/date.js
@@ -0,0 +1,10 @@
+// this script will determine the date and write it on the #date ID 
+        const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+        "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+      const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+      const d = new Date();
+      const currentMonth = months[d.getMonth()];
+      const currentDay = days[d.getDay()];
+      const currentDate = d.getDate();
+      const currentYear = d.getFullYear();
+      document.getElementById("date").innerHTML = `${currentDay}, ${currentMonth} ${currentDate}, ${currentYear}`;
